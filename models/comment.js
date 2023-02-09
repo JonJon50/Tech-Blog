d91f23f9602f4319b88bddfda2a0987c883e8fd5
John Hagens <ginuwine104@gmail.com>
2023-02-08 20:22:37 -0500

    comment.js: user.id id date

---
+++ models/comment.js
@@ -0,0 +1,25 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connections');
+
+class Comment extends Model { }
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
